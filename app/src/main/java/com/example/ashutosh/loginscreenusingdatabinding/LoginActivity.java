f0991c57d80e92f492bb267b59f7d00b7f2f7708
Ashutosh <ashutoshmca13@gmail.com>
2018-10-02 23:48:24 +0530

    connecting data binding with activity

--- app/src/main/java/com/example/ashutosh/loginscreenusingdatabinding/LoginActivity.java
+++ app/src/main/java/com/example/ashutosh/loginscreenusingdatabinding/LoginActivity.java
@@ -1,13 +1,18 @@
 package com.example.ashutosh.loginscreenusingdatabinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ashutosh.loginscreenusingdatabinding.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        ActivityLoginBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding.setUser(new User());
+
     }
 }
